ecc306ae295e84dfcfb78bd1333a1cd5d42ab67b
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2022-04-04 10:02:01 +0200

    Create goal.py

---
+++ goal.py
@@ -0,0 +1,21 @@
+import rsk
+from get_angle import *
+
+zone_goal_gauche= [ # coté vert (c1 et c2)
+  (-0.6, 0.45), # poteaux supérieur, (x,y)
+  (-0.6,-0.45)  ] # poteaux inférieur, (x,y)
+zone_goal_droit= [
+  (0.6, 0.45),
+  (0.6, -0.45)  ]  
+
+
+def goal(pos_balle,pos_robot,robot):
+    if pos_balle[0] + pos_robot[0] < 0.05:
+        robot.kick()
+    if pos_balle[0] >= 0.85:
+        x = -0.75
+        y = pos_balle[1] /2
+    else:
+        x = -0.7
+        y = pos_balle[1]
+    return (x,y)
