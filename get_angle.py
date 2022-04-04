4ac93a9ce4d414f9c4e532eb5ae2a2dac73eee8d
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2022-04-04 08:59:52 +0200

    Update get_angle.py

--- get_angle.py
+++ get_angle.py
@@ -1,15 +1,15 @@
-# N'a pas été testé
-from math import atan, pi, sqrt,
+from math import atan, pi, sqrt
 
 
-goal_droit= [
-  (0, 0), # poteaux supérieur, (x,y)
-  (0,0)  ] # poteaux inférieur, (x,y)
+goal_droit= [ # coté vert (c1 et c2)
+  (0.9, 0.3), # poteaux supérieur, (x,y)
+  (0.9,-0.3)  ] # poteaux inférieur, (x,y)
 goal_gauche= [
-  (0, 0),
-  (0,0)  ]
+  (-0.9, 0.3),
+  (-0.9, -0.3)  ]
 
 terrain_x = [-0.9, 0.9]
+terrain_y = [-0.6, 0.6]
 
 def get_alpha(pos_balle:tuple, goal:list):
   """
@@ -23,8 +23,8 @@ def get_alpha(pos_balle:tuple, goal:list):
   up = (goal[0][0]-pos_balle[0])**2
   down_1 = (pos_balle[1]-goal[0][1])**2
   down_2 = (pos_balle[1]-goal[1][1])**2
-  return atan ( sqrt( up / down_1 ) ), 
-         atan ( sqrt( up / down_2 ) )
+  return (atan ( sqrt( up / down_1 ) ), 
+        atan ( sqrt( up / down_2 ) ))
   
 def get_theta_att(pos_balle:tuple, cote:str):
   """
@@ -39,19 +39,19 @@ def get_theta_att(pos_balle:tuple, cote:str):
   if cote.lower() == "d":
     alpha = get_alpha(pos_balle, goal_droit)
     if pos_balle[1] > 0:
-      return - ( pi/2 - alpha[0]),
-             - ( pi/2 - alpha[1]),
+      return (- ( pi/2 - alpha[0]),
+             - ( pi/2 - alpha[1]))
     else:
-      return pi/2 - alpha[0],
-              pi/2 - alpha[1]
+      return (pi/2 - alpha[0],
+              pi/2 - alpha[1])
   elif cote.lower() == "g":
     alpha = get_alpha(pos_balle, goal_gauche)
     if pos_balle[1] > 0:
-      return - ( pi/2 + alpha[0]),
-              - ( pi/2 + alpha[1])
+      return (- ( pi/2 + alpha[0]),
+              - ( pi/2 + alpha[1]))
     else:
-      return pi/2 + alpha[0],
-              pi/2 + alpha[1]
+      return (pi/2 + alpha[0],
+              pi/2 + alpha[1])
     
 def get_theta_def(pos_balle:tuple, cote:str):
     """
@@ -63,19 +63,23 @@ def get_theta_def(pos_balle:tuple, cote:str):
     - cote : str - Côté que l'on attaque. d pour droite, g pour gauche 
                     ( par rapport au sens de l'axe des abscisses ) 
   """
-  if cote.lower() == "d":
-    alpha = get_alpha(pos_balle, goal_droit)
-    if pos_balle[1] > 0:
-      return pi/2 - alpha[0],
-              pi/2 - alpha[1]
-    else:
-      return -(pi/2 - alpha[0]),
-              -(pi/2 - alpha[1])
-  elif cote.lower() == "g":
-    alpha = get_alpha(pos_balle, goal_gauche)
-    if pos_balle[1] > 0:
-      return pi/2 + alpha[0],
-              pi/2 + alpha[1]
-    else:
-      return -(pi/2 + alpha[0]),
-              -(pi/2 + alpha[1])
+    if cote.lower() == "d":
+        alpha = get_alpha(pos_balle, goal_droit)
+        if pos_balle[1] > 0:
+            return (pi/2 - alpha[0],
+                    pi/2 - alpha[1])
+        else:
+            return (-(pi/2 - alpha[0]),
+                    -(pi/2 - alpha[1]))
+    elif cote.lower() == "g":
+        alpha = get_alpha(pos_balle, goal_gauche)
+        if pos_balle[1] > 0:
+            return (pi/2 + alpha[0],
+                    pi/2 + alpha[1])
+        else:
+            return (-(pi/2 + alpha[0]),
+                    -(pi/2 + alpha[1]))
+
+
+if __name__=="__main__":
+    pass
