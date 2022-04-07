a1700a854d2907a6f58d9ffaacd2d5e9378e95c2
Corentin-LAFORGUE <102510459+Corentin-LAFORGUE@users.noreply.github.com>
2022-04-07 15:42:13 +0200

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -1,14 +1,17 @@
 import rsk
 from metrics import *
+from math import pi
 
-def stay(robot) :
-  x = robot.position[0]
-  y = robot.position[1]
-  if y >= 0.5 :
-    robot.goto(x, 0.5)
-  if y <= -0.5 :
-    robot.goto(x, 0.5)
-  if x >= 0.6 :
-    robot.goto(0.6, y)
-  if x <= -0.6 :
-    robot.goto(-0.6, y)
+def rester_att(robot) :
+  while True :
+    x = robot.position[0]
+    y = robot.position[1]
+    o = robot.orientation
+    if y > 0.5 :
+      r_a = robot.goto((x, 0.5, o), wait=False)
+    elif y < -0.5 :
+      r_a = robot.goto((x, -0.5, o), wait=False)
+    elif x > 0.5 :
+      r_a = robot.goto((0.5, y, o), wait=False)
+    elif x < -0.5 :
+      r_a = robot.goto((-0.5, y, o), wait=False)
