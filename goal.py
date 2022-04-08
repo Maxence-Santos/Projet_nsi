ca495069fced884506e217ef77921fabf2bfa4c0
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2022-04-08 16:44:02 +0200

    Update goal.py

--- goal.py
+++ goal.py
@@ -1,4 +1,5 @@
 import rsk
+from math import pi, sqrt,radians
 from metrics import *
 
 zone_goal_gauche= [ # coté vert (c1 et c2)
@@ -8,14 +9,35 @@ zone_goal_droit= [
   (0.6, 0.45),
   (0.6, -0.45)  ]  
 
+d = False
 
-def goal(pos_balle,pos_robot,robot):
-    if pos_balle[0] + pos_robot[0] < 0.05:
-        robot.kick()
-    if pos_balle[0] >= 0.85:
-        x = -0.75
-        y = pos_balle[1] /2
+def goal(pos_balle,robot):
+    pos_robot = robot.position
+    print(pos_robot[0] - pos_balle[0])
+    if d:
+        #distance = sqrt((pos_balle[0] - pos_robot[0])**2 + (pos_balle[1] - pos_robot[1]) **2)
+        if abs(pos_robot[0] - pos_balle[0]) < 0.12: #distance < 0.11:
+            robot.kick()
+        a = sum(list(get_theta_def(pos_balle,"g"))) / 2
+        if pos_balle[0] <= 0.6 :
+            x = 0.8
+            y = pos_balle[1] /2
+        else:
+            x = pos_balle[0]
+            y = pos_balle[1]
     else:
-        x = -0.7
-        y = pos_balle[1]
-    return (x,y)
+        """distance = sqrt((pos_balle[0] - pos_robot[0])**2 + 
+            (pos_balle[1] - pos_robot[1])**2)
+        if distance < 0.05:
+            robot.kick()"""
+        #distance = sqrt((pos_balle[0] - pos_robot[0])**2 + (pos_balle[1] - pos_robot[1]) **2)
+        if abs(pos_robot[0] - pos_balle[0]) < 0.12: #distance < 0.11:
+            robot.kick()
+        a = sum(list(get_theta_def(pos_balle,"d"))) / 2
+        if pos_balle[0] >= -0.6 :
+            x = -0.8
+            y = pos_balle[1] /2
+        else:
+            x = -0.7
+            y = pos_balle[1]
+    robot.goto((x,y,a), wait=False)
