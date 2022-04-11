9f927b04d2449d4dbff91f7dc7aacc0196971b3a
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2022-04-11 10:03:51 +0200

    Update and rename celebrations to celebrations.py

--- celebrations.py
+++ celebrations.py
@@ -11,13 +11,13 @@ with rsk.Client(host='172.19.66.163', key='') as client:
     def taper_dans_les_mains(r1, r2):
         a = False
         while not a :
-            a1 = r1.goto((0.5, 0.1, (3*pi/2)), wait=False)
-            a2 = r2.goto((0.5, -0.1, (pi/2)), wait=False)
+            a1 = r1.goto((-0.5, 0.1, (3*pi/2)), wait=False)
+            a2 = r2.goto((-0.5, -0.1, (pi/2)), wait=False)
             a = a1 and a2
         r1.kick()
         r2.kick()
     
-    def spirale_individuelle(r1, r2):
+    def spirale_groupe(r1, r2):
         a = False
         while not a :
             a1 = r1.goto((-0.5, 0.3, 0), wait=False)
@@ -33,3 +33,10 @@ with rsk.Client(host='172.19.66.163', key='') as client:
             a1 = r1.goto((-0.5, 0.3, 0), wait=False)
             a2 = r2.goto((-0.5, -0.3, 0), wait=False)
             a = a1 and a2
+    
+    def tour_centre(r1, r2):
+        pass
+    
+    taper_dans_les_mains(robotBleu1, robotBleu2)
+    spirale_groupe(robotBleu1, robotBleu2)
+    tour_centre(robotBleu1, robotBleu2)
