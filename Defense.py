a3994f5e791543c9e4b502bbaf4f26a3dd860250
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2022-04-11 10:05:06 +0200

    Update Defense.py

--- Defense.py
+++ Defense.py
@@ -2,15 +2,22 @@ import rsk
 from math import pi, sqrt,radians
 from metrics import *
 
-d = False
+d = True
 
 def pos_def(pos_balle,pos_robot_adv,robot):
     pos_robot = robot.position
     if abs(pos_robot_adv[0] - pos_balle[0]) < 0.12:
-        if not d:
+        if not d and pos_balle[1] >= 0:
             a = sum(list(get_theta_def(pos_balle,"g"))) / 2
-            robot.goto((pos_balle[0] - 0.05,pos_balle[1] - 0.05,a))
+            robot.goto((pos_balle[0] + 0.2,pos_balle[1] - 0.1,a))
+        elif not d and  pos_balle[1] < 0:
+            a = sum(list(get_theta_def(pos_balle,"g"))) / 2
+            robot.goto((pos_balle[0] + 0.2,pos_balle[1] + 0.1,a))
         
         else:
-            a = sum(list(get_theta_def(pos_balle,"d"))) / 2
-            robot.goto((pos_balle[0],pos_balle[1],a))
+            if d and pos_balle[1] >= 0:
+                a = sum(list(get_theta_def(pos_balle,"d"))) / 2
+                robot.goto((pos_balle[0] - 0.2,pos_balle[1] - 0.1,a))
+            elif d and pos_balle[1] < 0:
+                a = sum(list(get_theta_def(pos_balle,"d"))) / 2
+                robot.goto((pos_balle[0] - 0.2,pos_balle[1] + 0.1,a))
