ddac5f5816540e64b62162fe643b12590ddb9620
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2022-04-13 23:06:33 +0200

    fix version control issues because im somewhat dumb

--- metrics.py
+++ metrics.py
@@ -1,48 +1,165 @@
-import rsk
-from os import system
-from metrics import *
-from goal import *
-import numpy as np
-from math import pi
+from math import atan, pi, sqrt, cos, sin
+from random import choice
+from numpy import linspace
 
-rsk.robot.Robot("/dev/rfcomm0")
+goal_droit= [ # coté vert (c1 et c2)
+  (0.9, 0.3), # poteaux supérieur, (x,y)
+  (0.9,-0.3)  ] # poteaux inférieur, (x,y)
+goal_gauche= [
+  (-0.9, 0.3),
+  (-0.9, -0.3)  ]
 
-def print_the_ball(client, dt):
-    print(client.ball)
+terrain_x = [-0.9, 0.9]
+terrain_y = [-0.6, 0.6]
 
-with rsk.Client(host='172.19.66.163', key='') as client:
-    robotBleu2 = client. robots['blue'][2]
-    robotBleu1 = client. robots['blue'][1]
-    pos_bal = client.ball
-    need_update = False
-    angle_a = None
-    while True:
-        """
-        if pos_bal[0] != client.ball[0] and pos_bal[1] != client.ball[1]:
-            pos_bal = client.ball
-            need_update = True
-            
-        verts = [client.robots['green'][1].pose,
-                client.robots['green'][2].pose]
-        bleus = [client.robots['blue'][1].pose,
-                client.robots['blue'][2].pose]
-        needed_distance = estimate_distance(pos_bal)
-        dist = get_distance(pos_bal, robotBleu1)
-        
-        if robotBleu1.pose[0] + (needed_distance - 0.05) > pos_bal[0]:
-            robotBleu1.goto((pos_bal[0] - 0.2 ,robotBleu1.pose[1], 0 ))
-            robotBleu1.goto((robotBleu1.pose[0],pos_bal[1], 0 ))
-        else:
-            
-            if angle_a is None or need_update:
-                angle_a = get_angle_att(pos_bal, "d", verts, robotBleu1.pose)
-                pos_att = get_pos_cadre(pos_bal, "d", angle_a)
-                need_update = False
-            #robotBleu1.goto((pos_att[0],pos_att[1], angle_a ), wait=False)
+distance_kick = 0.11
 
-        if dist < needed_distance  :
-            robotBleu1.kick()
-            """
-        client.blue1.leds(255,255,255)
-        print(f"Distance : {dist:.3f}, estimation : {needed_distance:.2f}")
-        
+def get_alpha(pos_balle:tuple, goal:list):
+  """
+  Retourne les deux angles alpha (voir les explications) qui serviront pour le calcul de theta.
+  Ceux-ci sont calculés en fonction des deux poteaux pour avoir les deux "extrémités" pour être correctement alignées.
+  
+  Paramètres:
+    - pos_balle : tuple - contient les positions x et y de la balle
+    - goal: list - contient les coordonnées des poteaux du goal 
+  """
+  up_1 = (pos_balle[1]-goal[0][1])**2
+  up_2 = (pos_balle[1]-goal[1][1])**2
+  down = (pos_balle[0]-goal[0][0])**2
+  return (atan ( sqrt( up_1 / down ) ), 
+        atan ( sqrt( up_2 / down ) ))
+
+def get_theta_att(pos_balle:tuple, cote:str):
+  """
+  Retourne les deux angles theta (voir les explications) pour que l'attaquant puisse cadrer.
+  Ceux-ci sont calculés en fonction des deux poteaux pour avoir les deux "extrémités" pour être correctement alignées.
+  
+  Paramètres:
+    - pos_balle : tuple - contient les positions x et y de la balle
+    - cote : str - Côté que l'on attaque. d pour droite, g pour gauche 
+                    ( par rapport au sens de l'axe des abscisses ) 
+    
+  """
+  angles = []
+  if cote.lower() == "d":
+    alphas = get_alpha(pos_balle, goal_droit)
+    for alpha, poteau in zip(alphas, goal_droit):
+      if pos_balle[1] > poteau[1]:
+        angles.append(-alpha)
+      else:
+        angles.append(alpha)
+  elif cote.lower() == "g":
+    alphas = get_alpha(pos_balle, goal_gauche)
+    for alpha, poteau in zip(alphas, goal_gauche):
+      if pos_balle[1] > poteau[1]:
+        angles.append(alpha + pi)
+      else:
+        angles.append(pi - alpha)
+  return angles
+
+def get_theta_vers_goal(pos_balle:tuple,cote:str, joueurs:list):
+  """
+  A faire
+  """
+  if cote.lower() == "d":
+    if joueurs[0][0] >  joueurs[1][0]:
+      joueur = joueurs[0]
+    else:
+      joueur = joueurs[1]
+  if cote.lower() == "g":
+    if joueurs[0][0] <  joueurs[1][0]:
+      joueur = joueurs[0]
+    else:
+      joueur = joueurs[1]
+  up = (pos_balle[1]-joueur[1])**2
+  down = (pos_balle[0]-joueur[0])**2
+  alpha = atan ( sqrt( up / down ) )
+  if cote.lower() == "d":
+    if pos_balle[1] > joueur[1]:
+      return - alpha, joueur
+    else:
+      return  alpha , joueur
+  elif cote.lower() == "g":
+    if pos_balle[1] > joueur[1]:
+      return alpha + pi, joueur
+    else:
+      return pi - alpha, joueur
+
+
+def get_angle_att(pos_balle:tuple, cote_attaque:str, joueurs:list, pos_att:list):
+  """Retourne l'angle que doit avoir l'attaquant
+  Args:
+      pos_balle (tuple)
+      cote_attaque (str): d ou g
+  Returns:
+      float
+  """
+  theta = get_theta_att(pos_balle, cote_attaque)
+  theta_adv, joueur = get_theta_vers_goal(pos_balle, cote_attaque, joueurs)
+  if min(theta) < theta_adv < max(theta):
+    if cote_attaque.lower() == "d" and joueur[0] > pos_balle[0] or cote_attaque.lower() == "g" and joueur[0] < pos_balle[0]:
+      thetas = [(theta[0], theta_adv), (theta[1], theta_adv)]
+
+      if abs(thetas[0][0] - thetas[0][1]) > abs(thetas[1][0] - thetas[1][1]):
+        theta = thetas[0]
+      else:
+        theta = thetas[1]
+
+  return sum(list(theta)) / 2
+
+def get_pos_cadre(pos_balle:tuple, cote_attaque:str, angle:float):
+  """Permet de pouvoir récupérer la position que doit avoir le joueur, en se décalant un peu de la balle pour ne pas lui rouler dessus 
+  Args:
+      pos_balle:tuple: Contient client.ball, la position de la balle
+  Returns:
+      (x,y)  tuple: Position vers laquelle le joueur doit aller
+  """
+  x_balle = pos_balle[0]
+  y_balle = pos_balle[1]
+
+  theta = angle # juste pour la lisibilité
+  x_hat = abs(0.09 * cos(theta))
+  y_hat = abs(0.09 * sin(theta))
+
+
+  if cote_attaque.lower() == "g":
+    if y_balle < 0:
+      x = x_balle + x_hat
+      y = y_balle - y_hat
+    else:
+      x = x_balle + x_hat
+      y = y_balle + y_hat
+  elif cote_attaque.lower() == "d":
+    if y_balle > 0:
+      x = x_balle - x_hat
+      y = y_balle + y_hat
+    else:
+      x = x_balle - x_hat
+      y = y_balle - y_hat
+
+  return (x,y)
+
+
+def get_angle_def(pos_balle:tuple, cote_attaque:str):
+  """Retourne l'angle que doit avoir le  goal
+  Args:
+      pos_balle (tuple)
+      cote_attaque (str): d ou g
+  Returns:
+      float
+  """
+  if cote_attaque.lower() == "d":
+      return sum(list(get_theta_att(pos_balle, "g"))) / 2 + pi
+  else:
+      return sum(list(get_theta_att(pos_balle, "d"))) / 2 + pi
+
+    
+
+def get_distance(pos_element:tuple,joueur):
+  """Est-ce que je dois vraiment faire une doc?"""
+  pos_joueur = joueur.pose
+  return sqrt((pos_element[0] - pos_joueur[0])**2 + 
+        (pos_element[1] - pos_joueur[1])**2)
+
+def estimate_distance(pos_bal):
+  return 0.02*pos_bal[0] + 0.11 # Trouvé par regression linéaire
--- script_brice.py
+++ script_brice.py
@@ -10,39 +10,50 @@ rsk.robot.Robot("/dev/rfcomm0")
 def print_the_ball(client, dt):
     print(client.ball)
 
+SIDE = "d"
+if SIDE == "d":
+    us = "blue"
+    them = "green"
+else:
+    us = "green"
+    them = "blue"
+
 with rsk.Client(host='172.19.66.163', key='') as client:
-    robotBleu2 = client. robots['blue'][2]
-    robotBleu1 = client. robots['blue'][1]
+    # Connection
+    robotBleu2 = client. robots[us][2]
+    robot1 = client. robots[us][1]
     pos_bal = client.ball
     need_update = False
     angle_a = None
     while True:
-        """
+        # MAJ valeurs
         if pos_bal[0] != client.ball[0] and pos_bal[1] != client.ball[1]:
             pos_bal = client.ball
             need_update = True
             
-        verts = [client.robots['green'][1].pose,
-                client.robots['green'][2].pose]
-        bleus = [client.robots['blue'][1].pose,
-                client.robots['blue'][2].pose]
+        them_pos = [client.robots[them][1].pose,
+                client.robots[them][2].pose]
+        us_pos = [client.robots[us][1].pose,
+                client.robots[us][2].pose]
         needed_distance = estimate_distance(pos_bal)
-        dist = get_distance(pos_bal, robotBleu1)
-        
-        if robotBleu1.pose[0] + (needed_distance - 0.05) > pos_bal[0]:
-            robotBleu1.goto((pos_bal[0] - 0.2 ,robotBleu1.pose[1], 0 ))
-            robotBleu1.goto((robotBleu1.pose[0],pos_bal[1], 0 ))
+        dist = get_distance(pos_bal, robot1)
+
+
+        # Déplacement attaquant
+        if (robot1.pose[0] + (needed_distance - 0.05) > pos_bal[0] and SIDE == "d") or \
+        (robot1.pose[0] + (needed_distance - 0.05) > pos_bal[0] and SIDE == "g"):
+            robot1.goto((pos_bal[0] - 0.2 ,robot1.pose[1], 0 ))
+            robot1.goto((robot1.pose[0],pos_bal[1], 0 ))
         else:
             
             if angle_a is None or need_update:
-                angle_a = get_angle_att(pos_bal, "d", verts, robotBleu1.pose)
-                pos_att = get_pos_cadre(pos_bal, "d", angle_a)
+                angle_a = get_angle_att(pos_bal, SIDE, them_pos, robot1.pose)
+                pos_att = get_pos_cadre(pos_bal, SIDE, angle_a)
                 need_update = False
-            #robotBleu1.goto((pos_att[0],pos_att[1], angle_a ), wait=False)
+            robot1.goto((pos_att[0],pos_att[1], angle_a ), wait=False)
 
         if dist < needed_distance  :
-            robotBleu1.kick()
-            """
-        client.blue1.leds(255,255,255)
+            robot1.kick()
+
         print(f"Distance : {dist:.3f}, estimation : {needed_distance:.2f}")
         
