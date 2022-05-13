af1e22ff06342f715164d48c5192f5f04be0c326
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2022-05-13 09:58:27 +0200

    Update Defense.py

--- Defense.py
+++ Defense.py
@@ -4,23 +4,24 @@ from metrics import *
 
 d = 0
 
-def pos_def(pos_balle,pos_robot_adv,robot):
-    pos_robot = robot.position
-    if abs(pos_robot_adv[0] - pos_balle[0]) < 0.15:
+def pos_def(pos_balle,pos_robot_adv,pos_goal,robot):
+    pos_robot_x = (pos_balle[0] + pos_goal[0]) / 2
+    pos_robot_y = (pos_balle[1] + pos_goal[1]) / 2
+    if abs(pos_robot_adv[0] - pos_balle[0]) < 0.15 and abs(pos_robot_adv[1] - pos_balle[1] < 0.03):
         if not d and pos_balle[1] >= 0 and pos_balle[0] < 0:
             a = sum(list(get_theta_def(pos_balle,"g"))) / 2
-            robot.goto((pos_balle[0] + 0.2,pos_balle[1] - 0.1,a))
+            robot.goto((pos_robot_x + 0.2,pos_robot_y - 0.1,a))
         elif not d and pos_balle[1] < 0 and pos_balle[0] < 0:
             a = sum(list(get_theta_def(pos_balle,"g"))) / 2
-            robot.goto((pos_balle[0] + 0.2,pos_balle[1] + 0.1,a))
+            robot.goto((pos_robot_x + 0.2,pos_robot_y + 0.1,a))
         
         else:
             if d and pos_balle[1] >= 0 and pos_balle[0] >= 0:
                 a = sum(list(get_theta_def(pos_balle,"d"))) / 2
-                robot.goto((pos_balle[0] - 0.2,pos_balle[1] - 0.1,a))
+                robot.goto((pos_robot_x - 0.2,pos_robot_y - 0.1,a))
             elif d and pos_balle[1] < 0 and pos_balle[0] >= 0:
                 a = sum(list(get_theta_def(pos_balle,"d"))) / 2
-                robot.goto((pos_balle[0] - 0.2,pos_balle[1] + 0.1,a))
+                robot.goto((pos_robot_x - 0.2,pos_robot_y + 0.1,a))
     
     else:
         robot.goto((0,0,0))
